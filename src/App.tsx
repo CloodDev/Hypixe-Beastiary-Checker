5a3cf42f5e269354b9163f881cfdc3a5579c5533
cloodowy <cloodowy@gmail.com>
2024-04-04 08:05:10 +0200

    Nie dziala ale nie wiem co

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function fetchPlayerData(username:string){
+  console.log(username)
+  const resp = fetch('https://api.hypixel.net/v2/player', {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+});
+  return resp
+}
 
+function App() {
+  const [data, setData] = useState({})
+  const nameBoxRef = useRef<HTMLInputElement>(null);
+  function handleIgnSubmit() {
+    if (nameBoxRef.current?.value) {
+      setData(fetchPlayerData(nameBoxRef.current?.value))
+    }
+  }
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === "Enter") {
+      handleIgnSubmit();
+      console.log(data);
+    }
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        className="nameBoxInput"
+        type="text"
+        placeholder="IGN"
+        ref={nameBoxRef}
+        onKeyDown={handleKeyDown}
+      />
+
     </>
   )
 }
